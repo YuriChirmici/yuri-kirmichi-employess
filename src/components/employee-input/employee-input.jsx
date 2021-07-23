a1d9a4b72ab153860f8657b0f519b8067f67d664
yuri_k <ykirmichi4@mail.ru>
2021-07-23 17:03:28 +0300

    added basic logic of the form

--- src/components/employee-input/employee-input.jsx
+++ src/components/employee-input/employee-input.jsx
@@ -1,11 +1,43 @@
+import React from 'react';
+
 import './employee-input.css';
 
 const EmployeeInput = (props) => {
+
+	const handleInputChange = (e) => {
+		e.preventDefault();
+
+		let reader = new FileReader();
+		let file = fileRef.current.files[0];
+
+		reader.readAsText(file);
+
+		reader.onload = function() {
+			//to get array of rows without spacebars 
+			//at the beginning and end
+			let arr = reader.result.split('\n').map(row => row.trim())
+			console.log(arr);
+		};
+
+		reader.onerror = function() {
+			console.log(reader.error);
+		};
+	}
+
+	const fileRef = React.createRef();
+
 	return (
 		<form action="">
-			<input type="file" />
+			<input 
+				type="file" 
+				name="sendEmployees"
+				ref={fileRef}
+			/>
 
-			<button className="btn btn-outline-primary">
+			<button 
+				className="btn btn-outline-primary"
+				onClick={handleInputChange}
+			>
 				get
 			</button>
 		</form>
@@ -13,3 +45,4 @@ const EmployeeInput = (props) => {
 }
 
 export default EmployeeInput;
+
